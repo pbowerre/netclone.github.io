75c3df951c0bdc06ed2bb9c63d9ee1c1971fbf11
pbowerre <pbowerre247@gmail.com>
2023-05-12 01:59:25 -0700

    first commit

--- srcs/js/main.js
+++ srcs/js/main.js
@@ -1,10 +1,9 @@
 
-    // let times = `<i class="fa fa-times"></i>`;
+    //selecting all the tags that will be given functionalities
     let plus = document.querySelectorAll(".innerInfo h3 i"),
     content = document.querySelectorAll(".innerInfo p");
 
-//    let ranks = [times, plus, content];
-
+    //function to loop over all the p tags in th faq sections
     for(let i = 0; i < plus.length; i+= 1){
             plus[i].addEventListener('click', function showContent(e){
                 fillers = e.target
@@ -20,36 +19,3 @@
                 // console.log(morers);
             });
     }
-
-    
-//    function showContent(){
-            
-//    }
-    
-   
-//    let btn = document.querySelector(".button");
-//    var txtInput = document.querySelector("input[type=text]").value;
-//    var txtPass = document.querySelector("input[type=password]").value;
-
-
-// btn.onclick =  function(){
-//    // btn.preventDefault();
-//    // alert("welcome to netflix site")
-  
-//    if(!txtInput || !txtPass){
-//        alert("Email and Password is required");
-//        return false;
-//    }else if(!txtInput.includes('@')){
-//        alert(" Email is not correct")
-//        return false;
-
-//    }
-   
-//    if(txtPass > 4){
-//        alert("Password must be greater than 4 ");
-//        return false;
-//    }else{
-//        console.log("Success")
-//    }
-// return true;
-// }
